5cd02b8b3d4c8cdb408395aa9e569ec738b70482
Arthur Michel <Arthur.michel1@viacesi.fr>
2018-05-30 13:32:41 +0200

    get Element XY for view get arraylit entity for view

--- Lorann/model/src/main/java/model/DAO/DataLoader.java
+++ Lorann/model/src/main/java/model/DAO/DataLoader.java
@@ -2,25 +2,31 @@ package model.DAO;
 
 import java.util.ArrayList;
 
+import model.Map;
 import model.elements.Element;
 import model.elements.Entity;
 
 public class DataLoader {
 	
 	private final static String sqlGetMapByID ="{call FindMapByID(?)}";
+	private Map map;
 	
-	public Element[][] getMapElementsByID(){
+	public DataLoader(Map map) {
+		this.map=map;
 		
-		
-		return null;
-	};
+	}
 	
 	
-public  ArrayList<Entity>  getMapEntityByID(){
-		
-		
-		return null;
-	}; 
+	public void setMapElementsByID(){};
+	public void setMapEntityByID(){};	
+	public void setMapHeroByID(){}
+
+
+	public void load() {
+		setMapElementsByID();
+		setMapEntityByID();
+		setMapHeroByID();
+	} 
 	
 	
 	
--- Lorann/model/src/main/java/model/Map.java
+++ Lorann/model/src/main/java/model/Map.java
@@ -19,5 +19,32 @@ private int width;
 private int height;
 private DataLoader dataLoader;
 
+public Map(){
+	this.dataLoader = new DataLoader(this);
+	this.dataLoader.load();
+	
+}
+
+public Element getElementXY(int x , int y) {return elements[x][y];}
+public ArrayList<Entity> getEntity(){return entities;}
+
+
+
+public Hero getHero(){return this.hero;};
+
+public boolean isFinished() {return finished;}
+
+public void setFinished(boolean finished) {this.finished = finished;}
+
+public int getID() {return ID;}
+
+public int getWidth() {return width;}
+
+public int getHeight(){return height;}
+
+
+
+
+
 
 }
