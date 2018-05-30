a4d802ed5cebc99965ee160fb378fc800d6f0f94
Arthur Michel <Arthur.michel1@viacesi.fr>
2018-05-30 11:40:26 +0200

    code bouchonée

--- Lorann/model/src/main/java/model/DAO/DataLoader.java
+++ Lorann/model/src/main/java/model/DAO/DataLoader.java
@@ -1,5 +1,33 @@
 package model.DAO;
 
+import java.util.ArrayList;
+
+import model.elements.Element;
+import model.elements.Entity;
+
 public class DataLoader {
+	
+	private final static String sqlGetMapByID ="{call FindMapByID(?)}";
+	
+	public Element[][] getMapElementsByID(){
+		
+		
+		return null;
+	};
+	
+	
+public  ArrayList<Entity>  getMapEntityByID(){
+		
+		
+		return null;
+	}; 
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
