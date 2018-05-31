1f38824153db38e2562ae7d0586ccd0d72b3be4e
Tank-White <33095440+Tank-White@users.noreply.github.com>
2018-05-31 12:00:58 +0200

    Play method implemented

--- Lorann/controller/src/main/java/controller/LorannController.java
+++ Lorann/controller/src/main/java/controller/LorannController.java
@@ -45,7 +45,48 @@ public class LorannController extends ControllerFacade implements IOrderPerforme
 	 * @see controller.ControllerFacade#play()
 	 */
 	@Override
-	public void play() {
+	public void play() throws InterruptedException {
+		while(this.getModel().getMap().getHero().isAlive()) {
+			Thread.sleep(TIME_SLEEP);
+			switch(this.userOrder) {
+			case UP:
+				this.getModel().getMap().getHero().moveUP();
+				break;
+			case DOWN:
+				this.getModel().getMap().getHero().moveDOWN();
+				break;
+			case LEFT:
+				this.getModel().getMap().getHero().moveLEFT();
+				break;
+			case RIGHT:
+				this.getModel().getMap().getHero().moveRIGHT();
+				break;
+			case TOP_LEFT:
+				this.getModel().getMap().getHero().moveUP_LEFT();
+				break;
+			case TOP_RIGHT:
+				this.getModel().getMap().getHero().moveUP_RIGHT();
+				break;
+			case BOTTOM_LEFT:
+				this.getModel().getMap().getHero().moveDOWN_LEFT();
+				break;
+			case BOTTOM_RIGHT:
+				this.getModel().getMap().getHero().moveDOWN_RIGHT();
+				break;
+			case MAGIC:
+				try {
+					this.launchFireball();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			case NOP:
+			default:
+				break;
+			}
+			this.clearUserOrder();
+			
+		}
+		this.getView().displayMessage("Game Over");
 		
 	}
 	
