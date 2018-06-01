711bf9ff292b67f68e29c2a3e0c954ef3a3a195f
Tank-White <33095440+Tank-White@users.noreply.github.com>
2018-06-01 10:16:42 +0200

    Tests case begin on LorannController

--- Lorann/controller/src/test/java/controller/LorannControllerTest.java
+++ Lorann/controller/src/test/java/controller/LorannControllerTest.java
@@ -2,14 +2,24 @@ package controller;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import model.IHero;
+import model.IModel;
+import view.IView;
+
 public class LorannControllerTest {
 	private LorannController lorannController;
+	private IView view;
+	private IModel model;
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -21,20 +31,32 @@ public class LorannControllerTest {
 
 	@Before
 	public void setUp() throws Exception {
+		lorannController = new LorannController(view, model);
+		
+	}
+	
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
-	@Test
-	public void testPlay() {
-		fail("Not yet implemented");
+	
+	@After
+	public void restoreStreams() {
+		System.setOut(System.out);
+		
 	}
 
 	@Test
-	public void testOrderPerform() {
-		fail("Not yet implemented");
+	public void testPlayGameOver() throws InterruptedException {
+		this.model.getMap().getHero().setAlive(false);
+		this.lorannController.play();
+		assertEquals("Game Over", outContent.toString());
+		
 	}
 	
 	@Test
