8c6a0a4002f2cb3a68eadbc52329da194dff1204
Arthur Michel <Arthur.michel1@viacesi.fr>
2018-06-04 17:18:34 +0200

    Fireball working :smirk:

--- Lorann/controller/src/main/java/controller/LorannController.java
+++ Lorann/controller/src/main/java/controller/LorannController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import model.IEntity;
 import model.IModel;
@@ -103,8 +104,9 @@ public class LorannController implements IOrderPerformer, IController {
 			
 				this.clearUserOrder();
 			
-			for (IEntity entity : model.getMap().getEntities()) {
-				entity.move();	
+			for (int i=0 ; i<model.getMap().getEntities().size();i++) {
+				
+				model.getMap().getEntities().get(i).move();
 			}
 			
 			
