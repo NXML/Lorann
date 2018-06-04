5edf7c8e7c3894990e8dc6a5371541237b57093c
Arthur Michel <Arthur.michel1@viacesi.fr>
2018-06-04 15:05:00 +0200

    useless

--- Lorann/view/src/main/java/view/Frame.java
+++ Lorann/view/src/main/java/view/Frame.java
@@ -126,7 +126,6 @@ public class Frame extends JFrame implements KeyListener , IView
 				
 			case KeyEvent.VK_NUMPAD5:
 				userOrder = UserOrder.MAGIC;
-				//FireBall fireball = new FireBall();
 				break;
 			
 			default:
