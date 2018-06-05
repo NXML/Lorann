0602d67796809f89196329178d1fd9d86e52f62c
Tank-White <33095440+Tank-White@users.noreply.github.com>
2018-06-05 16:32:13 +0200

    Test Suite added.

---
+++ Lorann/model/src/test/java/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ model.AllTests.class, model.elements.AllTests.class })
+public class AllTests {
+
+}
---
+++ Lorann/model/src/test/java/model/elements/AllTests.java
@@ -0,0 +1,11 @@
+package model.elements;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DoorTest.class, ElementTest.class, EntityTest.class, HeroTest.class, PurseTest.class })
+public class AllTests {
+
+}
