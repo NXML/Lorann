fbe6e275bdaa89c5409aa78ad6239bf35d9c943d
Tank-White <33095440+Tank-White@users.noreply.github.com>
2018-06-05 16:20:15 +0200

    Correction in PurseTest.

--- Lorann/model/src/test/java/model/elements/PurseTest.java
+++ Lorann/model/src/test/java/model/elements/PurseTest.java
@@ -2,40 +2,24 @@ package model.elements;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import model.ElementFactory;
-
+/**
+ * <h1>The Class PurseTest.</h1>
+ * 
+ * @author Nicolas
+ * @version 1.0
+ * @see Purse
+ */
 public class PurseTest {
 	
-	/**
-	 * <h1>The Class ElementFactoryTest.</h1>
-	 * 
-	 * @author Nicolas
-	 * @version 1.0
-	 * @see ElementFactory
-	 */
-	
-	
-	/** The value */
+	/** The value. */
 	private int value;
 	
-	/** The purse for the test */
+	/** The purse for the test. */
 	private Purse purse;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	
 	/**
 	 * Set up the test configuration.
 	 * 
@@ -46,22 +30,17 @@ public class PurseTest {
 	public void setUp() throws Exception {
 		this.purse = new Purse(value, value, null);
 		this.value = 100;
+		
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	
 	/**
-	 * 
-	 * Test of the getValue method
-	 * 
+	 * Test of the method getValue().
 	 */
 	@Test
 	public void testGetValue() {
 		final int expected = 100;
 		assertEquals(expected, purse.getValue());
+		
 	}
 
 }
