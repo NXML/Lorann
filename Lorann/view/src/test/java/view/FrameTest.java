2223d35b8aeff6d59f23e26acdcef3d10207baa7
Tank-White <33095440+Tank-White@users.noreply.github.com>
2018-06-05 16:27:20 +0200

    Correction in FrameTest.

--- Lorann/view/src/test/java/view/FrameTest.java
+++ Lorann/view/src/test/java/view/FrameTest.java
@@ -2,46 +2,29 @@ package view;
 
 import static org.junit.Assert.*;
 
-import java.awt.event.KeyEvent;
-
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import model.IModel;
-
+/**
+ * <h1>The Class FrameTest.</h1>
+ * 
+ * @author Nicolas
+ * @version 1.0
+ * @see Frame
+ */
 public class FrameTest {
 	
-	/**	 The width	 */
+	/**	The width .*/
 	private int width;
 	
-	/**	 The height	 */
+	/**	The height. */
 	private int height;
 	
-	/**	 The keyCode	 */
+	/**	The key code. */
 	private int keyCode;
 	
-	/**	 The message	 */
+	/** The message. */
 	private String message;
-	
-	/** The Frame to test. */
-	private Frame frame;
-	
-	private int X;
-	
-	private int Y;
-	
-	private int userorder;
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
 
 	/**
 	 * Set up the test configuration.
@@ -49,7 +32,6 @@ public class FrameTest {
 	 * @throws Exception
 	 * 			Signals that an exception has occurred
 	 */
-	
 	@Before
 	public void setUp() throws Exception {
 		this.width = 10;
@@ -59,30 +41,19 @@ public class FrameTest {
 		
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	
 	/**
-	 * 
-	 * Test of the getWidth method
-	 * 
+	 * Test of the method getWidth().
 	 */
 	@Test
 	public void testGetWidth() {
 		final int expected = 10;
 		assertEquals(expected, this.width);
+		
 	}
 	
-
-/**
- * 
- * Test of the getHeight method
- * 
- */
-
-
+	/**
+	 * Test of the method getHeight().
+	 */ 
 	@Test
 	public void testGetHeight() {
 		final int expected = 5;
@@ -90,21 +61,13 @@ public class FrameTest {
 
 	}
 
-
-/**
- * 
- * Test of the DisplayMessage method
- * 
- */
-
-
+	/**
+	 * Test of the method displayMessage().
+	 */
 	@Test
 	public void testDisplayMessage() {
 		final String expected = "Hello";
 		assertEquals(expected, this.message);
 	}
-
-
-
 
 }
