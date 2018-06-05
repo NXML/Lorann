14387402a95e3ce05ddd6afc8ab90dbcb5bf3386
Tank-White <33095440+Tank-White@users.noreply.github.com>
2018-06-05 11:11:30 +0200

    Javadoc generated for Controller and Class ElementFactoryTest added.

---
+++ Lorann/model/test/model/ElementFactoryTest.java
@@ -0,0 +1,59 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * <h1>The Class ElementFactoryTest.</h1>
+ * 
+ * @author Loïc
+ * @version 1.0
+ * @see ElementFactory
+ */
+public class ElementFactoryTest {
+	
+	/** */
+	private ElementFactory
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetVWall() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHWall() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCross() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGround() {
+		fail("Not yet implemented");
+	}
+
+}
